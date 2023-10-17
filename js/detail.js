715de97f6496de07c884c4e1172fa1a7ccc9cb6d
claudiomr79 <claudiomr79@gmail.com>
2023-10-17 17:36:36 -0300

    localstorge andando , detalle andando, ver footer y cantidad no se actualiza

--- js/detail.js
+++ js/detail.js
@@ -1,2 +1,93 @@
-localStorage.getItem('producto');
-console.log(producto + "estoy en detail");
+class Producto {
+    constructor(id, title, description, price, category, image) {
+        this.id = id;
+        this.title = title;
+        this.description = description;
+        this.price = price;
+        this.category = category;
+        this.image = image;
+    }
+}
+
+var cantidad = (JSON.parse(localStorage.getItem('producto'))).cantidad;
+console.log(cantidad);
+const productoElegido = JSON.parse(localStorage.getItem('producto'));
+//console.log(productoElegido + "estoy en detail");
+
+//console.log(productoElegido);
+let detalleProducto = new Producto(productoElegido.id, productoElegido.title, productoElegido.description, productoElegido.price, productoElegido.category, productoElegido.image);
+
+let mostrarDetalle = (detalleProducto) => {
+    const detail = document.getElementById('detail');
+    let html = '';
+    detail.innerHTML = html;
+    html += `
+        <div class="card">
+            <img src="${detalleProducto.image}" class="card-img-top" alt="producto">
+            <div class="card-body">
+                <div class="card-detail">
+                    <h5 class="card-title">${detalleProducto.title}</h5>
+                    <p class="card-text" id="description">${detalleProducto.description}</p>
+                    <p class="card-text">$${detalleProducto.price}</p>
+                </div>
+                <div class="card-cuantity">
+                    <button class="btn" id="-">-</button>
+                    <p>${ cantidad }</p>
+                    <button class="btn" id="+">+</button>
+                </div>
+                <button class="btn" id="btn_back">Volver</button>
+                <button class="btn" id="btn_add_cart_${detalleProducto.id}">Agregar al carrito</button>
+            </div>
+        </div>
+        `;
+    detail.innerHTML = html;
+    document.body.appendChild(detail);
+
+
+}
+
+document.body.addEventListener('click', (e) => {    
+    if (e.target.id.includes('btn_back')) {
+        window.location.href = '../index.html';
+    }
+    if (e.target.id.includes('btn_add_cart_')) {
+        let id = e.target.id.split('_')[3];
+        //console.log(id);
+        let producto = JSON.parse(localStorage.getItem('producto'));
+        let carrito = JSON.parse(localStorage.getItem('carrito'));
+        let productoEnCarrito = carrito.find(producto => producto.id == id);
+        if (productoEnCarrito) {
+            productoEnCarrito.cantidad += cantidad;
+        } else {
+            producto.cantidad = cantidad;
+            carrito.push(producto);
+            console.log(carrito);
+        }
+        localStorage.setItem('carrito', JSON.stringify(carrito));
+        //console.log(carrito);
+        window.location.href = '../index.html';
+    }
+});
+
+document.body.addEventListener('click', (e) => {
+    if (e.target.id.includes('-')) {
+        let cantidad = document.querySelector('.card-cuantity p');
+        if (cantidad.innerHTML > 1) {
+            cantidad.innerHTML--;
+            cantidad--;
+        }
+    }
+});
+
+document.body.addEventListener('click', (e) => {
+    if (e.target.id.includes('+')) {
+        let cantidad = document.querySelector('.card-cuantity p');
+        if (cantidad.innerHTML < 10) {
+            cantidad.innerHTML++;
+            cantidad++;
+        }
+    }
+
+});
+
+mostrarDetalle(detalleProducto);
--- js/index.js
+++ js/index.js
@@ -1,20 +1,16 @@
-class Producto {
-    constructor(id, title, description, price, category, image) {
-        this.id = id;
-        this.title = title;
-        this.description = description;
-        this.price = price;
-        this.category = category;
-        this.image = image;
-    }
-}
+
 
 function getProducto(id) {
     let data = fetch(`https://fakestoreapi.com/products/${id}`)
         .then(res => res.json())
-        .then(json => console.log(json))
+        .then(producto => {
+            //console.log(producto);
+            producto.cantidad = 10;
+            localStorage.setItem('producto', JSON.stringify(producto));
+            //ir a pagina detail.html
+            window.location.href = './pages/detail.html';
+        })
         .catch(err => console.log(err));
-        return data;
         
 }
 
@@ -31,7 +27,7 @@ let data = fetch('https://fakestoreapi.com/products')
                         <img src="${producto.image}" class="card-img-top" alt="...">
                         <div class="card-body">
                              <h5 class="card-title">${producto.title}</h5>
-                             <p class="card-text">${producto.description}</p>
+                             <p id="desc" class="card-text">${producto.description}</p>
                              <button class="btn" id="btn_view_more_${producto.id}">Ver mas</button>
                         </div>
                     </div>
@@ -46,16 +42,21 @@ document.body.addEventListener('click', (e) => {
     
     if (e.target.id.includes('btn_view_more_')) {
         let id = e.target.id.split('_')[3];
-        
-        productoSeleccionado = getProducto(id);
-        const producto = new Producto(productoSeleccionado.id, productoSeleccionado.title, productoSeleccionado.description, productoSeleccionado.price, productoSeleccionado.category, productoSeleccionado.image);
-        localStorage.setItem('producto', JSON.stringify(producto));
+        getProducto(id);
     }
 });
 
 
+const crearCarrito = () => {
+    let carrito = [];
+    localStorage.setItem('carrito', JSON.stringify(carrito));
+};
 
-    
+document.addEventListener('DOMContentLoaded', () => {
+    if (localStorage.getItem('carrito') == null) {
+        crearCarrito();
+    }
+});    
 
 
 
