b1c8b61ac7ed078cfd1d52ae000d0296c56c4829
Claudio Rohr <claudiomr79@gmail.com>
2023-10-16 21:41:44 -0300

    localstorage y clase producto

--- js/index.js
+++ js/index.js
@@ -1,4 +1,23 @@
-import { showDetail } from '../js/detail.js';
+class Producto {
+    constructor(id, title, description, price, category, image) {
+        this.id = id;
+        this.title = title;
+        this.description = description;
+        this.price = price;
+        this.category = category;
+        this.image = image;
+    }
+}
+
+function getProducto(id) {
+    let data = fetch(`https://fakestoreapi.com/products/${id}`)
+        .then(res => res.json())
+        .then(json => console.log(json))
+        .catch(err => console.log(err));
+        return data;
+        
+}
+
 
 let data = fetch('https://fakestoreapi.com/products')
     .then(res => res.json())
@@ -13,7 +32,7 @@ let data = fetch('https://fakestoreapi.com/products')
                         <div class="card-body">
                              <h5 class="card-title">${producto.title}</h5>
                              <p class="card-text">${producto.description}</p>
-                             <button class="btn" id="btn_view_more_${producto.id}"><a href="../pages/detail.html">Ver mas</a></button>
+                             <button class="btn" id="btn_view_more_${producto.id}">Ver mas</button>
                         </div>
                     </div>
                     `
@@ -24,11 +43,21 @@ let data = fetch('https://fakestoreapi.com/products')
     .catch(err => console.log(err));
 
 document.body.addEventListener('click', (e) => {
+    
     if (e.target.id.includes('btn_view_more_')) {
         let id = e.target.id.split('_')[3];
-        showDetail(id);
+        
+        productoSeleccionado = getProducto(id);
+        const producto = new Producto(productoSeleccionado.id, productoSeleccionado.title, productoSeleccionado.description, productoSeleccionado.price, productoSeleccionado.category, productoSeleccionado.image);
+        localStorage.setItem('producto', JSON.stringify(producto));
     }
 });
+
+
+
+    
+
+
 
 
 
